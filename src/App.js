9feac5604ec54f76ed2c8179beaf7b3c6eb27f95
ritish <sritish.10@gmail.com>
2024-03-22 17:30:29 +0530

    email test

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ritish Reddy
-        
-      </header>
+      <h1>Email Me</h1>
+      <a href="mailto:sritish.10@gmail.com?Subject=Your%20subject&Body=Don't%20change%20the%20subject">Contact us</a>
     </div>
   );
 }
